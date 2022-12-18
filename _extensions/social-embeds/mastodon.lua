7294303781668745d8a8936f265bdca724a0f65e
Mark Sellors <github@5vcc.com>
2022-12-18 01:23:34 +0000

    Removes requirement for two input values

--- _extensions/social-embeds/mastodon.lua
+++ _extensions/social-embeds/mastodon.lua
@@ -1,9 +1,8 @@
 function mastodon(args)
     if quarto.doc.isFormat('html') then
         local status_url = pandoc.utils.stringify(args[1])
-        local status_id = pandoc.utils.stringify(args[2])
         -- Assemble HTML to be returned
-        local html = '<div style="position: relative; padding-bottom: 56.25%; height: 0;"> <iframe src="'.. status_url ..'/'..status_id..'/embed" class="mastodon-embed" style="max-width: 100%; border: 0;" width="100%" height="400" allowfullscreen="allowfullscreen"></iframe><script src="https://mastodon.social/embed.js" async="async"></script></div> '
+        local html = '<div style="position: relative; padding-bottom: 56.25%; height: 0;"> <iframe src="'.. status_url ..'/embed" class="mastodon-embed" style="max-width: 100%; border: 0;" width="100%" height="400" allowfullscreen="allowfullscreen"></iframe><script src="https://mastodon.social/embed.js" async="async"></script></div> '
 
 	return pandoc.RawInline('html', html)
     else
